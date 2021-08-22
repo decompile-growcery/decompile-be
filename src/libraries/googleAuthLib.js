3fa85cff52af212efe2270cda8fc39af87927275
fbunaren <fransiscusbunaren@gmail.com>
2021-08-22 20:15:53 +0700

    Init Mailer Library

--- src/libraries/googleAuthLib.js
+++ src/libraries/googleAuthLib.js
@@ -1,4 +1,5 @@
-/* Some parts are adapted from https://medium.com/@jackrobertscott/how-to-use-google-auth-api-with-node-js-888304f7e3a0 */
+/* Some parts are adapted from 
+https://medium.com/@jackrobertscott/how-to-use-google-auth-api-with-node-js-888304f7e3a0 */
 const google = require('googleapis').google;
 
 class googleAuthLib{
@@ -29,7 +30,32 @@ class googleAuthLib{
 
 	// Google Plus API
 	getGooglePlusApi(auth) {
-		return google.plus({ version: 'v1', auth });
+		const service = google.people({
+			version: "v1",
+			auth
+		});
+		service.people.connections.list({
+				resourceName: "people/me",
+				pageSize: 10,
+				personFields: "names,emailAddresses"
+			},
+			(err, res) => {
+				if (err) return console.error("The API returned an error: " + err);
+				const connections = res.data.connections;
+				if (connections) {
+					console.log("Connections:");
+					connections.forEach(person => {
+						if (person.names && person.names.length > 0) {
+							console.log(person.names[0].displayName);
+						} else {
+							console.log("No display name found for connection.");
+						}
+					});
+				} else {
+					console.log("No connections found.");
+				}
+			}
+		);
 	}
 
 	getURL(){	
@@ -44,6 +70,7 @@ class googleAuthLib{
 	}
 
 	async getGoogleAccount(code){
+		//FIXME: Find the Latest API to get Email & Account Info from Google Auth Callback Code
 		// Retrieve Token
 		const data = await this.auth.getToken(code);
 		const tokens = data.tokens;
