2e088c2a91f13c1ab32b250a6953d45994bb4622
fbunaren <fransiscusbunaren@gmail.com>
2021-08-25 10:10:23 +0700

    Complete the Shopping Cart Backend

--- src/controllers/cartItemController.js
+++ src/controllers/cartItemController.js
@@ -4,7 +4,8 @@ const CartItem = db.cartitem;
 
 const addCartItem = (req, res) => {
     // Check if data is complete
-    if (!req.params.id) {
+	product_id = req.params.product_id;
+    if (!product_id) {
         res.status(400).send({
             status: "Failed",
             message: "Incomplete Data Provided"
@@ -14,7 +15,7 @@ const addCartItem = (req, res) => {
 	
     const cart_item = {
         user_id: req.user.id,
-        product_id: req.params.id
+        product_id: product_id
     }
 
     CartItem.findOrCreate({
@@ -58,36 +59,64 @@ const addCartItem = (req, res) => {
 
 const removeCartItem = (req, res) => {
     // Check if data is complete
-    if (!req.params.id) {
+	product_id = req.params.product_id
+    if (!product_id) {
         res.status(400).send({
             status: "Failed",
-            message: "Incomplete Data Provided"
+            message: "Product ID is not provided in the URL"
         });
         return;
     }
-	
-    CartItem.destroy({
-        where: { product_id: req.params.id }
-      })
-    .then(data => {
-        if (data == 1) {
-          res.send({
-            status: "Success",
-            message: "Product has been removed"
-          });
-        } else {
-          res.send({
-            status: "Failed",
-            message: `Cannot remove Product with id=${id}`
-          });
-        }
-      })
-    .catch(err => {
-		console.trace(err.message);
-		res.status(500).send({
-			message: "Error occurred while removing product"
-		});
-    });
+	CartItem.findOne({where: {product_id: product_id}})
+	.then(data => {
+		/* If quantity <= 1, then delete the item from cart
+		else just decrease the quantity */
+		if (data.quantity > 1){
+			// Decrease the quantity
+			CartItem.decrement('quantity', { by: 1, where: { id: data.id }})
+			.then(data => {
+				res.send({
+					status: "Success",
+					message: "Product has been removed"
+				  });
+			})
+			.catch(err => {
+				res.status(500).send({
+					status: "Success",
+					message: "Failed to remove product"
+				  });
+			})
+		}else{
+			// Delete the product from cart
+			CartItem.destroy({
+				where: { product_id: product_id }
+			  })
+			.then(data => {
+				if (data == 1) {
+				  res.send({
+					status: "Success",
+					message: "Product has been removed"
+				  });
+				} else {
+				  res.send({
+					status: "Failed",
+					message: `Cannot remove Product with id=${id} from Cart`
+				  });
+				}
+			  })
+			.catch(err => {
+				console.trace(err.message);
+				res.status(500).send({
+					message: "Error occurred while removing product from cart"
+				});
+			})
+		}
+	}).catch(err => {
+		res.send({
+			status: "Failed",
+			message: "Product is not found in Cart"
+		  });
+	});
 }
 
 const getCartItems = (req, res) => {
@@ -96,6 +125,7 @@ const getCartItems = (req, res) => {
         res.json({
             status: "Success",
             data: data,
+			product_count: data.length,
 			message: data.length + " different products found"
         });
     })
@@ -103,6 +133,7 @@ const getCartItems = (req, res) => {
       res.json({
 		status: "Success",
         data: {},
+		product_count: 0,
 		message: "0 different products found"
       });
     });
--- src/routes/cartItemRoutes.js
+++ src/routes/cartItemRoutes.js
@@ -2,8 +2,8 @@ const verifyAuth = require('../middlewares/verifyAuth');
 const cartItem = require('../controllers/cartItemController');
 var router = require("express").Router();
 
-router.post("/cart/add/:id", verifyAuth.verifyToken , cartItem.addCartItem);
+router.post("/cart/add/:product_id", verifyAuth.verifyToken , cartItem.addCartItem);
 router.get("/cart", verifyAuth.verifyToken , cartItem.getCartItems);
-router.delete("/cart/remove/:id", verifyAuth.verifyToken , cartItem.removeCartItem);
+router.delete("/cart/remove/:product_id", verifyAuth.verifyToken , cartItem.removeCartItem);
 
 module.exports = router;
