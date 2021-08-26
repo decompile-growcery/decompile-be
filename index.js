fb7574d2638a192a7fd7512d69dc8ea59e52234c
fbunaren <fransiscusbunaren@gmail.com>
2021-08-26 08:54:14 +0700

    Move message

--- index.js
+++ index.js
@@ -47,10 +47,8 @@ db.sequelize.sync();
 // PORT CONFIG
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
+	app.get('/',function(req, res){
+		res.send("Growcery Backend is up and running...");
+	})
     console.log(`App running on port ${port}`)
-})
-
-// Just A message
-app.get('/',function(req, res){
-	res.send("Growcery Backend is up and running...");
 })
