0d23b5d7d97713e461251b82f42732d7714e04ab
fbunaren <fransiscusbunaren@gmail.com>
2021-08-29 20:07:49 +0700

    Update Payment API

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -43,7 +43,7 @@ const createPayment = (req, res) => {
 		payment_status = api_response.status
 		paypal_payment_id = api_response.id
 
-		response_data = {paypal_payment_id: paypal_payment_id, status: payment_status, checkout_url: api_response.links[1].href}
+		response_data = {payment_status: payment_status, checkout_url: api_response.links[1].href}
 
 		const payment_data = {
 			user_id: req.user.id,
@@ -53,6 +53,7 @@ const createPayment = (req, res) => {
 		
 		Payment.create(payment_data)
 			.then(data => {
+				response_data.payment_id = data.id;
 				res.json({status: "Success",data: response_data,  message: "Payment has successfully been registered"});
 			})
 			.catch(err => {
@@ -66,34 +67,131 @@ const createPayment = (req, res) => {
 
 const getPaymentData = (req, res) => {
     // Check if data is complete
-	paypal_payment_id = req.params.paypalPaymentID
-	if (!paypal_payment_id) {
+	payment_id = req.params.payment_id
+	if (!payment_id) {
         res.status(400).send({
             status: "Failed",
-            message: "Missing paypal_payment_id"
+            message: "Missing payment_id"
         });
         return;
     }
 
-	// Retrieve Data from Paypal
-	let paypal_api_url = 'https://api.sandbox.paypal.com/v2/checkout/orders/' + paypal_payment_id;
+	Payment.findOne({where: {user_id: req.user.id , id: payment_id}})
+	.then(data => {
+		paypal_payment_id = data.paypal_payment_id;
+		// Retrieve Data from Paypal
+		let paypal_api_url = 'https://api.sandbox.paypal.com/v2/checkout/orders/' + paypal_payment_id;
 
-	let auth = {auth: {
-					username: process.env.PAYPAL_CLIENT_ID,
-					password: process.env.PAYPAL_SECRET
+		let auth = {auth: {
+						username: process.env.PAYPAL_CLIENT_ID,
+						password: process.env.PAYPAL_SECRET
+						}
 					}
-				}
 
-	axios.get(paypal_api_url,auth )
-	.then((api_res) => {
-		api_response = api_res.data;
-		delete api_response.links;
-		res.json({status: "Success",data: api_response,  message: "Payment data has successfully been retrieved"});	
+		axios.get(paypal_api_url,auth )
+		.then((api_res) => {
+			api_response = api_res.data;
+			// It does not need to be deleted, 
+			// but I delete it anyway to shorten the API response
+			delete api_response.links;
+			res.json({status: "Success",data: api_response,  message: "Payment data has successfully been retrieved"});	
+		})
+		.catch((err) => {
+			res.status(500).json({status: "Failed", message: err.message || "Failed to retrieve payment data"})
+		})
 	})
-	.catch((err) => {
-		res.status(500).json({status: "Failed", message: err.message || "Failed to retrieve payment data"})
+	.catch(err => {
+		res.status(400).json({status: "Failed", message: err.message || "Payment data not found for the given payment_id and user"})
+	});
+}
+
+const capturePaypalPayment = (req, res) => {
+    // Check if data is complete
+	payment_id = req.params.payment_id
+	if (!payment_id) {
+        res.status(400).send({
+            status: "Failed",
+            message: "Missing payment_id"
+        });
+        return;
+    }
+
+	Payment.findOne({where: {user_id: req.user.id , id: payment_id}})
+	.then(data =>{
+		paypal_payment_id = data.paypal_payment_id;
+		// Capture Paypal Checkout Order
+		let paypal_api_url = 'https://api.sandbox.paypal.com/v2/checkout/orders/' + paypal_payment_id + '/capture';
+
+		let auth = {auth: {
+						username: process.env.PAYPAL_CLIENT_ID,
+						password: process.env.PAYPAL_SECRET
+						},
+					headers: {
+						'Content-Type': 'application/json'
+						}
+					}
+
+		axios.post(paypal_api_url,{},auth )
+		.then((api_res) => {
+			api_response = api_res.data;
+
+			res.json({status: "Success",data: api_response,  message: "Payment has successfully been processed"});
+		})
+		.catch((err) => {
+			res.status(500).json({status: "Failed", message: "Failed to process the payment"})
+		})
+	})
+	.catch(error => {
+		res.status(500).json({status: "Failed", message: "Failed to process the payment"})
 	})
 }
+
+const refundPaypalPayment = (req, res) => {
+	//FIXME: Does it work?
+    // Check if data is complete
+	payment_id = req.params.payment_id;
+	if (!payment_id) {
+        res.status(400).send({
+            status: "Failed",
+            message: "Missing payment_id"
+        });
+        return;
+    }
+
+	Payment.findOne({where: {user_id: req.user.id , id: payment_id}})
+	.then(data => {
+		paypal_payment_id = data.paypal_payment_id;
+		
+		// Capture Paypal Checkout Order
+		let paypal_api_url = 'https://api.sandbox.paypal.com/v2/payments/captures/' + paypal_payment_id + '/refund';
+
+		let auth = {auth: {
+						username: process.env.PAYPAL_CLIENT_ID,
+						password: process.env.PAYPAL_SECRET
+						},
+					headers: {
+						'Content-Type': 'application/json'
+						}
+					}
+
+		axios.post(paypal_api_url,{},auth )
+		.then((api_res) => {
+			api_response = api_res.data;
+			res.json({status: "Success",data: api_response,  message: "Payment refund has been processed."});
+		})
+		.catch((err) => {
+			console.error (err.message);
+			res.status(500).json({status: "Failed", message: "Failed to process payment refund"})
+		})
+	})
+	.catch(err => {
+		console.error (err.message);
+		res.status(500).json({status: "Failed", message: "Failed to process payment refund"})
+	})
+
+	
+}
+
 
 const getCheckoutURL = (req, res) => {
     // Check if data is complete
@@ -109,8 +207,11 @@ const getCheckoutURL = (req, res) => {
 	res.json({status: "Success",data: "https://www.sandbox.paypal.com/checkoutnow?token=" + paypal_payment_id,  message: "Paypal Checkout URL has successfully been retrieved"});
 }
 
+
 module.exports = {
     createPayment,
 	getPaymentData,
-	getCheckoutURL
+	getCheckoutURL,
+	capturePaypalPayment,
+	refundPaypalPayment
 }
--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -3,7 +3,9 @@ const Payment = require('../controllers/paymentController');
 var router = require("express").Router();
 
 router.post("/payment/create", verifyAuth.verifyToken , Payment.createPayment);
-router.get("/payment/data/:paypalPaymentID", verifyAuth.verifyToken , Payment.getPaymentData);
-router.get("/payment/checkout-url/:paypalPaymentID", verifyAuth.verifyToken , Payment.getCheckoutURL);
+router.get("/payment/data/:payment_id", verifyAuth.verifyToken , Payment.getPaymentData);
+router.get("/payment/checkout-url/:payment_id", verifyAuth.verifyToken , Payment.getCheckoutURL);
+router.post("/payment/capture/:payment_id", verifyAuth.verifyToken , Payment.capturePaypalPayment);
+router.post("/payment/refund/:payment_id", verifyAuth.verifyToken , Payment.refundPaypalPayment);
 
 module.exports = router;
