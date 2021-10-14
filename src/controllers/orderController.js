a8641dbba2651012dc5b6623a46d829d42d1470a
Tsamara Esperanti Erwin <sarah.esperanti@gmail.com>
2021-10-14 14:34:01 +0700

    add count order id

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -136,7 +136,8 @@ const getOrdersByFarmer = async (req, res) => {
         var query2 = `SELECT o.id as order_id, o.address_id, o.status_id, s.status, o.total_price, o.total_weight, o.shipping_cost, o.is_delivery,
         oi.id as order_item_id, oi.product_id, oi.amount, oi.note, oi.weight, oi.price, 
         p.id as product_id, p.product_name, p.unit_weight, p.unit_name, pi.image, 
-        u.id as user_id, u.first_name, u.last_name, a.city, a.state, a.postal_code, a.street_address
+        u.id as user_id, u.first_name, u.last_name, a.city, a.state, a.postal_code, a.street_address,
+        count(o.id) as "count_order"
         FROM orders o
         JOIN order_status s ON s.id = o.status_id
         JOIN order_item oi ON o.id = oi.order_id
