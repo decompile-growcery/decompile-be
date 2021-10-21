6ffdab51d5ce3fd0f56e51d490f95478bfc9cee7
Tsamara Esperanti Erwin <sarah.esperanti@gmail.com>
2021-10-21 20:32:10 +0700

    change remove cart item on checkout

--- src/middlewares/removeFromCartAfterOrder.js
+++ src/middlewares/removeFromCartAfterOrder.js
@@ -6,36 +6,46 @@ const removeProductFromCart = (req, res) => {
     console.log(req.body.product)
     products = req.body.product 
     for (let i = 0; i < products.length; i++) {
-        CartItem.findOne({where: {product_id: products[i].product_id}})
-        .then(data => {
-            if (data.quantity > 1){
-                CartItem.decrement('quantity', 
-                { by: products[i].amount, 
-                    where: 
-                    { id: data.id }})
-                .catch(err => {
-                    res.status(500).send({
-                        status: "Success",
-                        message: err.message || "Failed to remove product"
-                      });
-                })
-            }else{
-                CartItem.destroy({
-                    where: { id: data.id }
-                  })
-                .then(data => {
-                    if (data != 1) {
-                      res.send({
-                        status: "Failed",
-                        message: `Cannot remove Product with id=${id} from Cart`
-                      });
-                  }})
-                .catch(err => {
-                    res.status(500).send({
-                        message: "Error occurred while removing product from cart"
-                    });
-                })
-            }
+        CartItem.findOne({where: {product_id: product_id}})
+	.then(data => {
+		if (data.quantity > 1){
+			CartItem.decrement('quantity', { by: 1, where: { id: data.id }})
+			.then(data => {
+				res.send({
+					status: "Success",
+					message: "Product has been removed"
+				  });
+			})
+			.catch(err => {
+				res.status(500).send({
+					status: "Success",
+					message: "Failed to remove product"
+				  });
+			})
+		}else{
+			CartItem.destroy({
+				where: { product_id: product_id }
+			  })
+			.then(data => {
+				if (data == 1) {
+				  res.send({
+					status: "Success",
+					message: "Product has been removed"
+				  });
+				} else {
+				  res.send({
+					status: "Failed",
+					message: `Cannot remove Product with id=${id} from Cart`
+				  });
+				}
+			  })
+			.catch(err => {
+				console.trace(err.message);
+				res.status(500).send({
+					message: "Error occurred while removing product from cart"
+				});
+			})
+		}
             if (i === products.length-1) {
                 res.send({
                     status: "Success",
--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -5,7 +5,7 @@ const removeFromCart = require('../middlewares/removeFromCartAfterOrder');
 var router = require("express").Router();
 
 router.post("/order", verifyAuth.verifyToken, payment.createPayment, 
-order.insertOrder, order.insertOrderItem, removeFromCart.removeProductFromCart,);
+order.insertOrder, order.insertOrderItem, removeFromCart.removeProductFromCart);
 router.get("/order", verifyAuth.verifyToken, order.getOrdersByUser);
 router.put("/order-status", verifyAuth.verifyToken, order.updateOrderStatus);
 router.put("/order/payment-received", verifyAuth.verifyToken, order.updateStatusConfirmed);
